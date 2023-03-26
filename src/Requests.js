fc1f962c2fb22d7aab9f80a9f762873fd7df9f2a
The-GO0d-Guy <malaha814@gmail.com>
2023-03-27 01:51:49 +0530

    commit message

---
+++ src/Requests.js
@@ -0,0 +1,10 @@
+const key = '791d876c1ddee5c61488124d94e4f4d5';
+
+const requests = {
+    requestPopular:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated:`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestUpcoming:`https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
